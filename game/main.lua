2b96b59dceea64e806275b1b3fd293bc2caf1bde
fernandotibia <148167367+fernandotibia@users.noreply.github.com>
2023-10-28 21:14:09 -0300

    Update main.lua

--- game/main.lua
+++ game/main.lua
@@ -18,87 +18,37 @@ local audio = require("audio")
 
 
 -- Configure the audio channels (adjust according to your needs)
-audio.reserveChannels(3)
+audio.reserveChannels(2)
 
 local backgroundMusic = audio.loadStream("/audio/short-8-bit-background-music-for-video-mobile-game-old-school-164704.mp3")
 local fireSound = audio.loadSound( "/audio/mixkit-big-fire-flame-burning-1331.mp3" )
-local explosionSound = audio.loadSound( "/audio/archivo.mp3" )
 
+-- Volume fixo para a música de fundo e para o som "fire" (intervalo de 0 a 1)
+local fixedVolume = 0.5
 
--- Set initial volumes for each audio track (from 0.0 to 1.0)
-local backgroundVolume = 0.6 -- 60% volume for background music
-local fireVolume = 1.0 -- 100% volume for fire sound
-local explosionVolume = 0.8 -- 80% volume for the explosion sound
-
+-- Inicie o Composer para gerenciar as cenas
 composer.gotoScene("game")
 
--- Function to play background music
-local function playBackgroundMusic()
-    audio.setVolume(backgroundVolume, { channel = 1 }) 
-    audio.play(backgroundMusic, { channel = 1, loops = -1 })
+-- Inicie a música de fundo quando o aplicativo começa
+local backgroundMusicChannel
+
+local function startBackgroundMusic()
+    backgroundMusicChannel = audio.play(backgroundMusic, { channel = 1, loops = -1, volume = fixedVolume })
 end
 
--- Function to play fire sound
+-- Função para reproduzir o som "fire" com volume fixo
 local function playFireSound()
-    audio.setVolume(fireVolume, { channel = 2 })
+    audio.setVolume(fixedVolume, { channel = 2 }) -- Define o volume do canal 2
     audio.play(fireSound, { channel = 2 })
 end
 
--- Function to play explosion sound
-local function playExplosionSound()
-    audio.setVolume(explosionVolume, { channel = 3 })
-    audio.play(explosionSound, { channel = 3 })
-end
-
-local function adjustBackgroundVolume(volume)
-    backgroundVolume = volume
-    audio.setVolume(backgroundVolume, { channel = 1 })
-end
-
--- Function to adjust the fire sound volume
-local function adjustFireVolume(volume)
-    fireVolume = volume
-    audio.setVolume(fireVolume, { channel = 2 })
-end
-
--- Function to adjust the volume of the explosion sound
-local function adjustExplosionVolume(volume)
-    explosionVolume = volume
-    audio.setVolume(explosionVolume, { channel = 3 })
-end
-
--- Example: Start playing background music when the game starts
-playBackgroundMusic()
-
--- Example: Create a button to increase the volume
-local increaseVolumeButton = display.newText("Increase Volume", display.contentCenterX, 100, native.systemFont, 24)
-
--- Add an event handler to the button
-increaseVolumeButton:addEventListener("tap", function(event)
-    local newVolume = backgroundVolume + 0.1
-    if newVolume <= 1.0 then
-        adjustVolume(newVolume)
-    end
-end)
-
--- Example: Create a button to decrease the volume
-local decreaseVolumeButton = display.newText("Decrease Volume", display.contentCenterX, 150, native.systemFont, 24)
-
--- Add an event handler to the button
-decreaseVolumeButton:addEventListener("tap", function(event)
-    local newVolume = backgroundVolume - 0.1
-    if newVolume >= 0.0 then
-        adjustVolume(newVolume)
-    end
-end)
-
+-- Quando alguma ação no jogo faz com que as árvores peguem fogo (por exemplo, uma colisão com um objeto inflamável)
 local function onTreesCatchFire()
+    -- Lógica para determinar que as árvores pegaram fogo (por exemplo, detecção de colisões)
+
+    -- Após determinar que as árvores pegaram fogo, chame a função para reproduzir o som "fire"
     playFireSound()
-    -- Logic for trees to catch fire
 end
 
--- Replace the example event or logic below
-local function onExplosion()
-    playExplosionSound()
-    adjustExplosionVolume(0.7)
-end
+-- Chame a função para iniciar a música de fundo
+startBackgroundMusic()
